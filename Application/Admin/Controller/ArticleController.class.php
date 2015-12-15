74ca8f40fbde27c081d3a8d02cc46055192cea8b
dongsongsong <819874746@qq.com>
2015-12-15 18:36:47 +0800

    Datatables插件的使用  数据列表修改

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -8,12 +8,19 @@ class ArticleController extends BaseController{
 	//为数据模型提供数据
 	public function ajaxList(){
 		$p = I('p');
-		$page = 3;
+		$page = 5;
 		$start = ($p-1)*$page;
 		$count=M("news")->where("status = 1")->order("id desc")->count();
 		$data['count']=$count;
 		$data['page'] = $count%$page==0?$count/$page:floor($count/$page)+1;
-		$data['list']=M("news")->where("status = 1")->order("id desc")->limit("$start,$page")->select();
+		$column= $_GET['order'][0]['column'];
+		$dir = $_GET['order'][0]['dir'];
+		if($column==1){
+			$ls = "id";
+		}else{
+			$ls = "time";
+		}
+		$data['list']=M("news")->where("status = 1")->order("$ls $dir")->limit("$start,$page")->select();
 		$this->ajaxReturn($data);
 	}
 	public function article_add(){
